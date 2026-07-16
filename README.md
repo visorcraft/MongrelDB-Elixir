ebfd069e9646f041cdc7f01e6c5fa167a5bdcaed
visorcraft <thomas@visorcraft.com>
2026-07-16 14:34:40 -0500

    bump bindings to 0.57.0

--- README.md
+++ README.md
@@ -24,7 +24,7 @@
 ```elixir
 def deps do
   [
-    {:mongreldb, "~> 0.55.0"}
+    {:mongreldb, "~> 0.57.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MongrelDB.MixProject do
   use Mix.Project
 
-  @version "0.55.0"
+  @version "0.57.0"
   @source_url "https://github.com/visorcraft/MongrelDB-Elixir"
   @homepage "https://www.mongreldb.com"
 
