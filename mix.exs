6363d9e53a03a55b11277b5a385c569b376abee5
visorcraft <29009015+visorcraft@users.noreply.github.com>
2026-07-18 08:34:42 -0500

    Release client train 0.60.2.
    Align package/library version and CI server download pins with MongrelDB
    engine release v0.60.2.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MongrelDB.MixProject do
   use Mix.Project
 
-  @version "0.59.1"
+  @version "0.60.2"
   @source_url "https://github.com/visorcraft/MongrelDB-Elixir"
   @homepage "https://www.mongreldb.com"
 
