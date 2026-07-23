6d029820b7ddff2512b0294bef9b6c8e674bdb86
visorcraft <thomas@visorcraft.com>
2026-07-23 13:41:52 -0500

    release 0.64.5
    Align client with MongrelDB 0.64.5 and MongrelDB Kit 0.64.5.

--- README.md
+++ README.md
@@ -24,7 +24,7 @@
 ```elixir
 def deps do
   [
-    {:mongreldb, "~> 0.64.4"}
+    {:mongreldb, "~> 0.64.5"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MongrelDB.MixProject do
   use Mix.Project
 
-  @version "0.64.4"
+  @version "0.64.5"
   @source_url "https://github.com/visorcraft/MongrelDB-Elixir"
   @homepage "https://www.mongreldb.com"
 
